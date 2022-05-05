24cdcba954422c5b15313335acf985796f4c6bb9
robustnessaiapp <104385175+robustnessaiapp@users.noreply.github.com>
2022-05-05 10:37:03 +0200

    Delete RQ3 directory

--- RQ3/convert.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-
-converter = tf.compat.v1.lite.TFLiteConverter.from_keras_model_file(model_file='patch_model.h5')
-converter.post_training_quantize = True
-
-
-tflite_model = converter.convert()
-open('patch_model.tflite', "wb").write(tflite_model)

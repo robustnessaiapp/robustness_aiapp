de80bd4e33078792ffdbf6b811a00dcb690dee51
robustnessaiapp <104385175+robustnessaiapp@users.noreply.github.com>
2022-05-05 10:37:19 +0200

    Delete RQ5 directory

--- RQ5/convert.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-
-converter = tf.compat.v1.lite.TFLiteConverter.from_keras_model_file(model_file='alexnet.h5')
-converter.post_training_quantize = True
-
-
-tflite_model = converter.convert()
-open('alexnet.tflite', "wb").write(tflite_model)
